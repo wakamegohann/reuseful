ab6d9c7d75f26175e3d14bed761aeccdf97ed9a7
kiyomasa <kiyomasagondo1120@gmail.com>
2025-04-04 14:58:11 +0900

    add websocketPort property to next.config.ts

--- front/next.config.ts
+++ front/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['local.masamasa.site'],
+  websocketPort: 3000
 };
 
 export default nextConfig;
